0ba3a6d343bd391a3367715e2d8f90a54d4622aa
andresecheverrijaramillo <68928458+andresecheverrijaramillo@users.noreply.github.com>
2023-08-24 17:13:10 -0500

    Add files via upload

---
+++ Sweetdreams/djangocourse/helloworld/pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import HomePageView, ProductIndexView, ProductShowView, ProductCreateView, ProductCreatedView
+urlpatterns = [
+    path("", HomePageView.as_view(), name='home'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/created', ProductCreatedView.as_view(), name='created'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+]
