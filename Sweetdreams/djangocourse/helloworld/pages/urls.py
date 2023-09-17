a785e6288fdbf36519f88b6dcd2839f94a599c8a
andresecheverrijaramillo <aecheverrj@eafit.edu.co>
2023-09-16 22:10:03 -0500

    log, sign y carrito

--- Sweetdreams/djangocourse/helloworld/pages/urls.py
+++ Sweetdreams/djangocourse/helloworld/pages/urls.py
@@ -1,10 +1,24 @@
 from django.urls import path
-from .views import HomePageView, ProductIndexView, ProductShowView, CartView, AddToCartView, CartRemoveAllView
+from .views import (
+    HomePageView,
+    ProductIndexView,
+    ProductShowView,
+    CartView,
+    AddToCartView,
+    CartRemoveAllView,
+    signup,
+    signout,
+    signin,
+)
+
 urlpatterns = [
-    path("", HomePageView.as_view(), name='home'),
-    path('products/', ProductIndexView.as_view(), name='index'),
-    path('products/<str:id>', ProductShowView.as_view(), name='show'),
-    path('cart/', CartView.as_view(), name='cart'),
-    path('cart/add/<str:product_id>', AddToCartView.as_view(), name='add_to_cart'),
-    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
+    path("", HomePageView.as_view(), name="home"),
+    path("products/", ProductIndexView.as_view(), name="index"),
+    path("products/<str:id>", ProductShowView.as_view(), name="show"),
+    path("cart/", CartView.as_view(), name="cart"),
+    path("cart/add/<str:product_id>", AddToCartView.as_view(), name="add_to_cart"),
+    path("cart/removeAll", CartRemoveAllView.as_view(), name="cart_removeAll"),
+    path("signup/", signup, name="signup"),
+    path("signout/", signout, name="signout"),
+    path("signin/", signin, name="signin"),
 ]
--- Sweetdreams/djangocourse/helloworld/pages/views.py
+++ Sweetdreams/djangocourse/helloworld/pages/views.py
@@ -5,48 +5,56 @@ from django.views import View
 from django.urls import reverse
 from django import forms
 from django.shortcuts import render, redirect, get_object_or_404
-from django.core.exceptions import ValidationError 
+from django.core.exceptions import ValidationError
 from .models import Product, Category, Organization
 from random import sample
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+
 
 # Create your views here.
 class HomePageView(TemplateView):
-    template_name = 'home.html'
+    template_name = "home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         # Obtén tres productos aleatorios de la base de datos
         random_products = sample(list(Product.objects.all()), 3)
-        context['title'] = 'Home Page - SweetDreams'
-        context['random_products'] = random_products  # Pasa los productos aleatorios al contexto
+        context["title"] = "Home Page - SweetDreams"
+        context[
+            "random_products"
+        ] = random_products  # Pasa los productos aleatorios al contexto
         return context
-    
+
+
 class ProductFilterForm(forms.Form):
     # Agrega los campos que desees para filtrar, por ejemplo, una categoría
     category = forms.ModelChoiceField(
         queryset=Category.objects.all(),
         empty_label="Todas las Categorías",
-        required=False
+        required=False,
     )
     organization = forms.ModelChoiceField(
         queryset=Organization.objects.all(),
         empty_label="Todas las Compañías",
-        required=False
+        required=False,
     )
 
+
 class ProductIndexView(View):
-    template_name = 'products/index.html'
+    template_name = "products/index.html"
 
     def get(self, request):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of products"
-        
+
         # Obtén el formulario de filtrado y procesa los datos enviados por el usuario
         filter_form = ProductFilterForm(request.GET)
         if filter_form.is_valid():
-            category = filter_form.cleaned_data.get('category')
-            organization = filter_form.cleaned_data.get('organization')
+            category = filter_form.cleaned_data.get("category")
+            organization = filter_form.cleaned_data.get("organization")
             # Filtra los productos en función de los parámetros del formulario
             products = Product.objects.all()
             if category:
@@ -56,15 +64,17 @@ class ProductIndexView(View):
         # Agrega los productos filtrados y el formulario al contexto
         viewData["products"] = products
         viewData["filter_form"] = filter_form
-        
+
         return render(request, self.template_name, viewData)
 
+
 class ProductShowView(View):
-    template_name = 'products/show.html'
+    template_name = "products/show.html"
+
     def get(self, request, id):
         try:
-            product_id=int(id)
-            if 1<= int(id):
+            product_id = int(id)
+            if 1 <= int(id):
                 viewData = {}
                 product = get_object_or_404(Product, pk=product_id)
                 viewData["title"] = product.name + " - Online Store"
@@ -72,47 +82,104 @@ class ProductShowView(View):
                 viewData["product"] = product
                 return render(request, self.template_name, viewData)
             else:
-                return HttpResponseRedirect(reverse('home'))
+                return HttpResponseRedirect(reverse("home"))
         except ValueError:
-            return HttpResponseRedirect(reverse('home'))
+            return HttpResponseRedirect(reverse("home"))
+
     def post(self, request, id):
-        product_id = request.POST.get('product_id')  # Obtén el ID del producto del formulario
+        product_id = request.POST.get(
+            "product_id"
+        )  # Obtén el ID del producto del formulario
         try:
             product = Product.objects.get(id=product_id)
             product.delete()
-            return redirect('home')
+            return redirect("home")
         except Product.DoesNotExist:
             pass  # Manejar el caso en que el producto no existe
-        return redirect('home')
-        
+        return redirect("home")
+
 
 class CartView(View):
-    template_name = 'cart/index.html'
+    template_name = "cart/index.html"
 
     def get(self, request):
         products = Product.objects.all()
-        cart_product_data = request.session.get('cart_product_data', {})
-        cart_products = [product for product in products if str(product.id) in cart_product_data.keys()]
-        cart_total = sum([product.price for product in cart_products])
+        cart_product_data = request.session.get("cart_product_data", {})
+        cart_products = [
+            product
+            for product in products
+            if str(product.id) in cart_product_data.keys()
+        ]
+
+        # Actualizar cart_products para incluir la cantidad
+        for product in cart_products:
+            product.quantity = cart_product_data[str(product.id)]
+
+        cart_total = sum(
+            [product.price * product.quantity for product in cart_products]
+        )
         view_data = {
-            'title': 'SweetDreams - Cart',
-            'subtitle': 'Shopping Cart',
-            'cart_products': cart_products,
-            'cart_total': cart_total
+            "title": "SweetDreams - Cart",
+            "subtitle": "Shopping Cart",
+            "cart_products": cart_products,
+            "cart_total": cart_total,
         }
         return render(request, self.template_name, view_data)
 
+
 class AddToCartView(View):
     def post(self, request, product_id):
-        product_id = request.POST.get('product_id')
-        cart_product_data = request.session.get('cart_product_data', {})
-        cart_product_data[product_id] = product_id
-        request.session['cart_product_data'] = cart_product_data
-        return redirect('index')
+        product_id = request.POST.get("product_id")
+        cart_product_data = request.session.get("cart_product_data", {})
+
+        # Incrementar la cantidad del producto en el carrito
+        cart_product_data[product_id] = int(cart_product_data.get(product_id, 0)) + 1
+
+        request.session["cart_product_data"] = cart_product_data
+        return redirect("index")
+
 
 class CartRemoveAllView(View):
     def post(self, request):
         # Remove all products from cart in session
-        if 'cart_product_data' in request.session:
-            del request.session['cart_product_data']
-        return redirect('cart')
+        if "cart_product_data" in request.session:
+            del request.session["cart_product_data"]
+        return redirect("cart")
+
+
+def signup(request):
+    if request.method == "GET":
+        form = UserCreationForm()
+        view_data = {"form": form}
+        return render(request, "sign/signup.html", view_data)
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
+                user.save()
+                login(request, user)
+                # Redirige al usuario a la página de registro con un parámetro "success"
+                return HttpResponseRedirect(f"{reverse('signup')}?success=1")
+            except Exception as e:
+                return HttpResponseRedirect(f"{reverse('signup')}?user=1")
+        return HttpResponseRedirect(f"{reverse('signup')}?password=1")
+def signout(request):
+    logout(request)
+    return redirect("home")
+
+def signin(request):
+    if request.method == "GET":
+        form = AuthenticationForm()
+        view_data = {"form": form}
+        return render (request,"sign/signin.html",view_data)
+    else:
+        user=authenticate(request, username=request.POST["username"],password=request.POST["password"])
+        if user is None:
+            return HttpResponseRedirect(f"{reverse('signin')}?error=1")
+        else:
+            login(request, user)
+            return HttpResponseRedirect(f"{reverse('signin')}?success=1")
+        
