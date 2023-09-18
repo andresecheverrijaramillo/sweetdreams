6102bc5e1b7a70db21873ff722f34aa2f2786f45
SantiagoGonzalezR <sgonzalez6@eafit.edu.co>
2023-09-18 00:23:29 -0500

    Fixed host IP

--- Sweetdreams/djangocourse/helloworld/helloworld_project/settings.py
+++ Sweetdreams/djangocourse/helloworld/helloworld_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ydx#90c54)3)+#@5!kky4i%u(67hyn%+95%+wg6w+s!jls$v&f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['35.233.74.49']
+ALLOWED_HOSTS = ['35.223.74.49']
 
 
 # Application definition
